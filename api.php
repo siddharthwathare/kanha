6353d5cb7b1be9b3e292576f916c6cf7f16f6dd5
siddharthwathare <138600240+siddharthwathare@users.noreply.github.com>
2026-04-21 16:55:11 +0530

    Update api.php

--- api.php
+++ api.php
@@ -4,9 +4,25 @@ header("Access-Control-Allow-Origin: *");
 
 $action = $_GET['action'] ?? '';
 
-if ($action == "products") {
+// 🔴 DB CONNECTION (TiDB)
+$conn = mysqli_init();
+$conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
 
-    $conn = new mysqli("sql205.infinityfree.com", "if0_41704812", "OX6joYEsffEpm", "if0_41704812_kanha");
+$conn->real_connect(
+    "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
+    "2RpDz4SXbM8nohL.root",
+    "2HOhPpMJzR5SZrg1",
+    "kanha",
+    4000
+);
+
+// Check connection
+if ($conn->connect_error) {
+    echo json_encode(["error" => $conn->connect_error]);
+    exit;
+}
+
+if ($action == "products") {
 
     $result = $conn->query("SELECT * FROM products");
 
@@ -17,7 +33,7 @@ if ($action == "products") {
     }
 
     echo json_encode($data);
-    exit; // 🔴 VERY IMPORTANT
+    exit;
 }
 
 echo json_encode(["error" => "Invalid action"]);
