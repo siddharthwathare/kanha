1ddda17b074713557333ac3273bdb29a9a660027
siddharthwathare <138600240+siddharthwathare@users.noreply.github.com>
2026-04-21 17:43:54 +0530

    Update api.php

--- api.php
+++ api.php
@@ -1,14 +1,3 @@
-$conn = mysqli_init();
-
-// 🔴 FORCE SSL
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-
-$conn->real_connect(
-    "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
-    "2RpDz4SXbM8nohL.root",
-    "2HOhPpMJzR5SZrg1",
-    "kanha",
-    4000,
-    NULL,
-    MYSQLI_CLIENT_SSL
-);
+<?php
+echo "API WORKING";
+exit;
